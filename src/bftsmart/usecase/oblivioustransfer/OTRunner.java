1ee3778c300b4230d4e4d6513294a608e29b51cb
Farzin <fhous001@ucr.edu>
2021-01-08 12:17:01 -0800

    test thread count

--- src/bftsmart/usecase/oblivioustransfer/OTRunner.java
+++ src/bftsmart/usecase/oblivioustransfer/OTRunner.java
@@ -4,11 +4,35 @@ import bftsmart.runtime.RMIRuntime;
 import bftsmart.usecase.PartitionedObject;
 
 public class OTRunner {
+    private static final Object lock = new Object();
+    private static int counter = 0;
+
+//    public static void main(String[] _args) {
+//        while (true) {
+//            new Thread(new Runnable() {
+//                public void run() {
+//                    synchronized(lock) {
+//                        counter++;
+//                        System.err.println("New thread #" + counter);
+//                    }
+//                    while (true) {
+//                        try {
+//                            Thread.sleep(3000);
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//            }).start();
+//        }
+//    }
     public static void main(String[] args) throws Exception {
         PartitionedObject object = new PartitionedObject();
         int i = 0;
         for(i = 0; i < object.getHosts().get(0).size(); i++) {
             int finalI = i;
+            counter++;
+            System.err.println("New thread #" + counter);
             new Thread(() -> {
                 try {
                     RMIRuntime.main(new String[]{String.valueOf(finalI), String.valueOf(1), "bftsmart.usecase.oblivioustransfer.OTA"});
@@ -19,6 +43,8 @@ public class OTRunner {
         }
         for(; i < object.getHosts().get(0).size() + object.getHosts().get(1).size(); i++) {
             int finalI = i;
+            counter++;
+            System.err.println("New thread #" + counter);
             new Thread(() -> {
                 try {
                     RMIRuntime.main(new String[]{String.valueOf(finalI), String.valueOf(2), "bftsmart.usecase.oblivioustransfer.OTB"});
@@ -31,5 +57,23 @@ public class OTRunner {
         RMIRuntime.main(new String[]{String.valueOf(i), String.valueOf(3), "bftsmart.usecase.oblivioustransfer.OTClient"});
 
 
+    }
+
+    public static int recommendedThreadCount()
+    {
+        int mRtnValue = 0;
+        Runtime runtime = Runtime.getRuntime();
+        long maxMemory = runtime.maxMemory();
+        long mTotalMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+        int mAvailableProcessors = runtime.availableProcessors();
+
+        long mTotalFreeMemory = freeMemory + (maxMemory - mTotalMemory);
+        mRtnValue = (int)(mTotalFreeMemory/4200000000l);
+
+        int mNoOfThreads = mAvailableProcessors-1;
+        if(mNoOfThreads < mRtnValue) mRtnValue = mNoOfThreads;
+
+        return mRtnValue;
     }
 }
